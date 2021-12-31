6043c3677c2045ef6ea3da302c98ef0421ae6a02
louib <code@louib.net>
2021-12-31 12:35:19 -0500

    Basic command name handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut command_name: String = "".to_string();
+    let args: Vec<String> = env::args().collect();
+    if args.len() >= 2 {
+        command_name = args[1].clone();
+    }
+
+    // Not sure this makes sense long term.
+    if command_name.len() == 0 {
+        println!("No command provided, defaulting to ls");
+        command_name = "ls".to_string();
+    }
+    println!("Executing command {}.", command_name);
+
+    if command_name == "ls" {}
+
+    // I should be able to list the valid command names here,
+    // or this should have been handled earlier?
+    panic!("Invalid command name {}", command_name);
 }
