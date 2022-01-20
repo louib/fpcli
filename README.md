70c166137205cde2b992975cf8bce808c2ca59dc
louib <code@louib.net>
2022-01-19 20:09:36 -0500

    Migrate CLI interface to clap

---
+++ README.md
@@ -0,0 +1,24 @@
+# fpcli
+A CLI app for Flatpak manifests.
+
+## Usage
+```
+fpcli
+A CLI app for Flatpak manifests.
+
+USAGE:
+    fpcli <SUBCOMMAND>
+
+OPTIONS:
+    -h, --help    Print help information
+
+SUBCOMMANDS:
+    get-urls    Get all the urls contained in a manifest
+    help        Print this message or the help of the given subcommand(s)
+    lint        Formats a Flatpak manifest
+    ls          List all the Flatpak manifests in a specific directory
+    to-yaml     Converts a manifest to YAML. The manifest must be a valid Flatpak manifest
+```
+
+## License
+MIT
--- src/main.rs
+++ src/main.rs
@@ -2,112 +2,174 @@ use std::env;
 use std::fs;
 use std::path;
 
-use flatpak_rs::application::FlatpakApplication;
+use clap::{AppSettings, Parser, Subcommand};
+use flatpak_rs::application::{FlatpakApplication, FlatpakManifestFormat};
 use flatpak_rs::module::{FlatpakModule, FlatpakModuleItem};
 
 mod bare_install;
 mod utils;
 
+/// A CLI app for Flatpak manifests.
+#[derive(Parser)]
+#[clap(name = "fpcli")]
+#[clap(about = "A CLI app for Flatpak manifests.", long_about = None)]
+struct Fpcli {
+    #[clap(subcommand)]
+    command: SubCommand,
+}
+
+#[derive(Subcommand)]
+enum SubCommand {
+    /// Formats a Flatpak manifest.
+    #[clap(setting(AppSettings::ArgRequiredElseHelp))]
+    Lint {
+        /// The path of the manifest to lint.
+        path: String,
+        /// Only check the manifest for formatting issues.
+        #[clap(long, short)]
+        check: bool,
+    },
+    /// List all the Flatpak manifests in a specific directory.
+    Ls {
+        /// The path of the directory to traverse.
+        path: String,
+    },
+    /// Get all the urls contained in a manifest.
+    #[clap(name = "get-urls")]
+    GetUrls {
+        /// The path of the manifest to parse.
+        path: String,
+    },
+    /// Converts a manifest to YAML. The manifest must be a valid
+    /// Flatpak manifest.
+    #[clap(setting(AppSettings::ArgRequiredElseHelp))]
+    #[clap(name = "to-yaml")]
+    ToYaml {
+        /// The path of the manifest to convert.
+        path: String,
+    },
+}
+
 fn main() {
-    let mut command_name: String = "".to_string();
-    let args: Vec<String> = env::args().collect();
-    if args.len() >= 2 {
-        command_name = args[1].clone();
-    }
+    let args = Fpcli::parse();
 
-    // Not sure this makes sense long term.
-    if command_name.len() == 0 {
-        println!("No command provided, defaulting to ls");
-        command_name = "ls".to_string();
-    }
-    println!("Executing command {}.", command_name);
+    match &args.command {
+        SubCommand::Ls { path } => {
+            // FIXME right now we don't use the path parameter.
+            for file_path in crate::utils::get_all_paths(path::Path::new(".")).unwrap() {
+                if !file_path.is_file() {
+                    continue;
+                }
 
-    if command_name == "get-urls" {
-        if args.len() <= 2 {
-            panic!("Please provide a file path to parse.");
-        }
-        let file_path = args[2].clone();
-
-        // TODO do some validations on the file path before trying to parse it.
-        let mut all_urls: Vec<String> = vec![];
-
-        if let Ok(flatpak_application) = FlatpakApplication::load_from_file(file_path.to_string()) {
-            for module in flatpak_application.get_all_modules_recursively() {
-                let module_description = match module {
-                    FlatpakModuleItem::Description(d) => d,
-                    FlatpakModuleItem::Path(_) => continue,
+                let file_path = match file_path.to_str() {
+                    Some(f) => f,
+                    None => continue,
                 };
-                for url in module_description.get_all_urls() {
-                    println!("{}", url);
+
+                if file_path.contains(".git/") {
+                    continue;
+                }
+
+                if let Ok(flatpak_application) =
+                    FlatpakApplication::load_from_file(file_path.to_string())
+                {
+                    println!("Flatpak application at {}.", &file_path);
                 }
             }
         }
+        SubCommand::GetUrls { path } => {
+            // TODO do some validations on the file path before trying to parse it.
+            let mut all_urls: Vec<String> = vec![];
 
-        if let Ok(flatpak_module) = FlatpakModule::load_from_file(file_path.to_string()) {
-            for url in flatpak_module.get_all_urls() {
-                println!("{}", url);
+            if let Ok(flatpak_application) = FlatpakApplication::load_from_file(path.to_string()) {
+                for module in flatpak_application.get_all_modules_recursively() {
+                    let module_description = match module {
+                        FlatpakModuleItem::Description(d) => d,
+                        FlatpakModuleItem::Path(_) => continue,
+                    };
+                    for url in module_description.get_all_urls() {
+                        println!("{}", url);
+                    }
+                }
             }
+
+            if let Ok(flatpak_module) = FlatpakModule::load_from_file(path.to_string()) {
+                for url in flatpak_module.get_all_urls() {
+                    println!("{}", url);
+                }
+            }
+
+            // TODO Also try to parse for source manifests.
         }
-
-        // TODO Also try to parse for source manifests.
-        return;
-    }
-
-    if command_name == "ls" {
-        for file_path in crate::utils::get_all_paths(path::Path::new(".")).unwrap() {
-            if !file_path.is_file() {
-                continue;
+        SubCommand::ToYaml { path } => {
+            if !path.ends_with(".json") {
+                panic!("Please provide the path of a .json manifest to convert.");
             }
 
-            let file_path = match file_path.to_str() {
-                Some(f) => f,
-                None => continue,
+            // TODO we should also try to parse the file as a module manifest or as a source manifest!
+            let mut flatpak_application = match FlatpakApplication::load_from_file(path.to_string())
+            {
+                Ok(m) => m,
+                Err(e) => {
+                    eprintln!("Could not parse manifest file at {}: {}.", path, e);
+                    return;
+                }
             };
 
-            if file_path.contains(".git/") {
-                continue;
-            }
+            flatpak_application.format = FlatpakManifestFormat::YAML;
 
-            if let Ok(flatpak_application) =
-                FlatpakApplication::load_from_file(file_path.to_string())
-            {
-                println!("Flatpak application at {}.", &file_path);
-            }
+            let application_dump = match flatpak_application.dump() {
+                Ok(d) => d,
+                Err(e) => {
+                    eprintln!("Could not dump manifest: {}.", e);
+                    return;
+                }
+            };
+
+            let yaml_file_path = path.replace(".json", ".yaml");
+
+            if let Err(e) = fs::write(path::Path::new(&yaml_file_path), application_dump) {
+                panic!("could not write file {}: {}.", yaml_file_path, e);
+            };
         }
-        return;
-    }
+        SubCommand::Lint { path, check } => {
+            // TODO we should also try to parse the file as a module manifest or as a source manifest!
+            let flatpak_application = match FlatpakApplication::load_from_file(path.to_string()) {
+                Ok(m) => m,
+                Err(e) => {
+                    eprintln!("Could not parse manifest file at {}: {}.", path, e);
+                    return;
+                }
+            };
 
-    if command_name == "lint" {
-        if args.len() <= 2 {
-            panic!("Please provide a file path to parse.");
+            let initial_content = match fs::read_to_string(path) {
+                Ok(c) => c,
+                Err(e) => {
+                    eprintln!("Could not read file {}: {}!", &path, e);
+                    return;
+                }
+            };
+
+            let application_dump = match flatpak_application.dump() {
+                Ok(d) => d,
+                Err(e) => {
+                    eprintln!("Could not dump manifest: {}.", e);
+                    return;
+                }
+            };
+
+            if *check {
+                if application_dump == initial_content {
+                    println!("The file is formatted correctly.");
+                    return;
+                } else {
+                    panic!("There are formatting issues with the file.");
+                }
+            }
+
+            if let Err(e) = fs::write(path::Path::new(&path), application_dump) {
+                panic!("could not write file {}: {}.", path, e);
+            };
         }
-        let file_path = args[2].clone();
-
-        // TODO we should also try to parse the file as a module manifest or as a source manifest!
-        let flatpak_application = match FlatpakApplication::load_from_file(file_path.to_string()) {
-            Ok(m) => m,
-            Err(e) => {
-                eprintln!("Could not parse manifest file at {}: {}.", file_path, e);
-                return;
-            }
-        };
-
-        let application_dump = match flatpak_application.dump() {
-            Ok(d) => d,
-            Err(e) => {
-                eprintln!("Could not dump manifest: {}.", e);
-                return;
-            }
-        };
-
-        if let Err(e) = fs::write(path::Path::new(&file_path), application_dump) {
-            panic!("could not write file {}: {}.", file_path, e);
-        };
-
-        return;
     }
-
-    // I should be able to list the valid command names here,
-    // or this should have been handled earlier?
-    panic!("Invalid command name {}", command_name);
 }
